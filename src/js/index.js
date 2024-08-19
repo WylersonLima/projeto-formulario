79d0fbd02a6253da677caef7f7050f0ce02aa769
Wylerson Lima <wylersonlima@icloud.com>
2024-08-19 10:21:57 -0400

    Primeiro Commit

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form');
+const input = document.querySelectorAll('.inputs');
+
+
+
+function validarFormulario(){
+  input.forEach(function(input){
+    addEventListener('submit', function(){
+      if(input.value === ""){
+        input.classList.add('requerido');
+        input.nextElementSibling.classList.add('hidden');
+      }else {
+        input.classList.add('preenchido');
+      };
+    });
+  });
+
+};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  validarFormulario();
+})
+
+
